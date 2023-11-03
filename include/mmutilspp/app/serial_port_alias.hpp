ace86688fddb102f8ad8a26ff9b20de36513a14f
FlowerFerry <422040095@qq.com>
2023-11-03 17:33:08 +0800

    -

--- include/mmutilspp/app/serial_port_alias.hpp
+++ include/mmutilspp/app/serial_port_alias.hpp
@@ -11,7 +11,7 @@
 
 namespace mmupp { namespace app {
     
-    struct serial_port_alias
+    struct serport_alias
     {
         enum replace_mode_t {
             unknown,
@@ -20,22 +20,36 @@ namespace mmupp { namespace app {
             external_shared,
         };
         
-        serial_port_alias():
-            replace_mode(unknown)
+        serport_alias():
+            replace_mode(unknown),
+            id(-1)
         {}
 
         memepp::string name;
         memepp::string path;
         std::unordered_set<memepp::string> types; //< RS232, RS485, RS422, ...
         replace_mode_t replace_mode;
+        int id;
     };
+    using serport_alias_ptr = std::shared_ptr<serport_alias>;
 
-    struct serial_port_alias_list
+    struct serport_alias_list
     {
-        std::vector<serial_port_alias> list;
-    };
-    using serial_port_alias_list_ptr = std::shared_ptr<serial_port_alias_list>;
+        serport_alias_ptr find(int _id) const;
 
+        std::vector<serport_alias_ptr> list;
+    };
+    using serport_alias_list_ptr = std::shared_ptr<serport_alias_list>;
+
+    inline serport_alias_ptr serport_alias_list::find(int _id) const
+    {
+        auto it = std::find_if(list.begin(), list.end(), 
+            [_id](const serport_alias_ptr& _alias) {
+                return _alias->id == _id;
+            });
+        return it != list.end() ? *it : nullptr;
+    }
+    
 }}
 
 #endif // !MMUPP_APP_SERIAL_PORT_ALIAS_HPP_INCLUDED
