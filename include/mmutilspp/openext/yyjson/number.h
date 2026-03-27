c93317c9fdd96a188e439148c93e074c4be72fa7
FlowerFerry <flower_ferry@outlook.com>
2026-03-27 14:49:07 +0800

    -

---
+++ include/mmutilspp/openext/yyjson/number.h
@@ -0,0 +1,36 @@
+
+#ifndef MMUPP_OPENEXT_NUMBER_H_INCLUDED
+#define MMUPP_OPENEXT_NUMBER_H_INCLUDED
+
+#include <yyjson.h>
+#include <meme/string_fwd.h>
+
+namespace mmupp{
+namespace openext {
+
+    template<typename T>
+    inline T get_num_with_default(yyjson_val *_val, const char* _key, mmint_t _slen, T _default) 
+    {
+        if (_slen < 0) 
+            _slen = strlen(_key);
+        yyjson_val *v = yyjson_obj_getn(_val, _key, static_cast<size_t>(_slen));
+        if (v == nullptr) 
+            return _default;
+            
+        switch (yyjson_get_type(v)) 
+        {
+        case YYJSON_TYPE_BOOL: 
+            return yyjson_get_bool(v) ? 1 : 0;
+        case YYJSON_TYPE_NUM: 
+            return static_cast<T>(yyjson_get_num(v));
+        case YYJSON_TYPE_STR: 
+            return static_cast<T>(std::stod(yyjson_get_str(v)));
+        default: 
+            return _default;
+        }
+    }
+
+}
+}
+
+#endif // !MMUPP_OPENEXT_NUMBER_H_INCLUDED
